1b9a0ec8a727381cbb25a4d30ae3771949b45d9e
Chris Whitehouse <chris_whitehouse@hotmail.com>
2020-12-10 16:29:38 +0000

    Test Helpers

--- spec/features/entering_names_spec.rb
+++ spec/features/entering_names_spec.rb
@@ -5,18 +5,12 @@ feature Battle do
   # end
 
   scenario 'see players names renders on screen' do
-    visit '/'
-    fill_in 'player_1_name', with: "Chris W"
-    fill_in 'player_2_name', with: "Chris A"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Chris W vs. Chris A"
   end
 
   scenario 'Player 1 can see Player 2 Hit points' do
-    visit '/'
-    fill_in 'player_1_name', with: "Chris W"
-    fill_in 'player_2_name', with: "Chris A"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Chris A: 60HP"
   end
 
