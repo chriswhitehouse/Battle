1e6949f3af36270e41d9d45caf6392e3508c0505
xavierloos <xavier.loos04@gmail.com>
2020-12-11 16:20:18 +0000

    Implementing HP

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,18 @@
 class Player
   attr_reader :name, :hp
+  REDUCE_HP = 10
+  DEFAULT_HP = 60
 
   def initialize(name)
     @name = name
-    @hp = 60
-  end
-  def reduce_hp
-    @hp -= 10
+    @hp = DEFAULT_HP
   end
 
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hp -= REDUCE_HP
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,22 @@
 describe Player do
-  let(:player) { Player.new("name") }
+  let(:player_one) { Player.new("Chris") }
+  let(:player_two) { Player.new("Javi") }
   it "should return its name" do
-    expect(player.name).to eq "name"
+    expect(player_one.name).to eq "Chris"
+  end
+
+  it "returning the HP" do
+    expect(player_one.hp).to eq described_class::DEFAULT_HP
+  end
+  describe "#Attack" do
+    it "damages the player" do
+      expect(player_two).to receive(:receive_damage)
+      player_one.attack(player_two)
+    end
+  end
+  describe "#Receive_damage" do
+    it "reduce the player HP" do
+      expect { player_one.receive_damage }.to change { player_one.hp }.by(-described_class::REDUCE_HP)
+    end
   end
 end
